ee1b81698fadc0ce70d09cc333cfc468f7b33966
pedrolibas <email@email.com>
2024-04-12 16:44:52 -0300

    feat: list

---
+++ src/scripts/data.js
@@ -0,0 +1,206 @@
+export const data = [
+  {
+    name: "Santo Agostinho",
+    caption: "A medida do amor é amar sem medida.",
+  },
+  {
+    name: "João Paulo II",
+    caption: "Ser Santo é lutar contra o pecado de todos os Dias",
+  },
+  {
+    name: "Santa Terezinha do Menino Jesus",
+    caption:
+      "Compreendi que o Amor englobava todas as vocações, que o Amor era tudo... ",
+  },
+  {
+    name: "Santa Terezinha",
+    caption: "Faça tudo com Amor, por Amor e para o Amor, esse é o segredo",
+  },
+  {
+    name: "Santa Teresinha do Menino Jesus",
+    caption: "Nada é pequeno se feito com amor...",
+  },
+  {
+    name: "Santa Rita de Cássia",
+    caption:
+      "Senhor, dá-me um amor capaz de entregar tudo por um amor sempre maior.",
+  },
+  {
+    name: "Santo Agostinho",
+    caption: "Necessitamos um do outro, para sermos nós mesmos. ",
+  },
+  {
+    name: "Santo Agostinho",
+    caption: "Conhece-te, aceita-te, supera-te. ",
+  },
+  {
+    name: "Santo Agostinho",
+    caption: "Nada estará perdido enquanto estivermos em busca. ",
+  },
+  {
+    name: "Santo Agostinho",
+    caption: "A oração é o encontro da sede de Deus e da sede do homem.",
+  },
+  {
+    name: "Santo Agostinho",
+    caption: "A oração é o encontro da sede de Deus e da sede do homem. ",
+  },
+  {
+    name: "São Maximiliano Kolbe",
+    caption:
+      "Deus nos envia os amigos para ser nosso firme apoio no redemoinho da luta. Na companhia dos amigos, encontraremos forças para atingir nosso sublime ideal.",
+  },
+  {
+    name: "Santo Agostinho",
+    caption:
+      "Ama e faz o que quiseres. Se calares, calarás com amor; se gritares, gritarás com amor; se corrigires, corrigirás com amor; se perdoares, perdoarás com amor. Se tiveres o amor enraizado em ti, nenhuma coisa senão o amor serão os teus frutos.",
+  },
+  {
+    name: "Madre Teresa de Calcutá",
+    caption: "É fácil amar os que estão longe. Mas nem sempre é fácil amar os que vivem ao nosso lado.",
+  },
+  {
+    name: "Santa Teresa de Ávila",
+    caption: "Uma prova de que Deus esteja conosco não é o fato de que não venhamos a cair, mas que nos levantemos depois de  cada queda.",
+  },
+  {
+    name: "Madre Teresa de Calcutá",
+    caption: "É fácil amar os que estão longe. Mas nem sempre é fácil amar os que vivem ao nosso lado",
+  },
+  {
+    name: "Santo Agostinho",
+    caption: "Ter fé é assinar uma folha em branco e deixar que Deus nela escreva o que quiser.",
+  },
+  {
+    name: "Santo Agostinho",
+    caption: "Fizeste-nos, Senhor, para ti, e o nosso coração anda inquieto enquanto não descansar em ti. ",
+  },
+  {
+    name: "Santo Agostinho",
+    caption: "Nada estará perdido enquanto estivermos em busca.",
+  },
+  {
+    name: "São Francisco de Assis",
+    caption: "Senhor fazei de mim um instrumento de vossa paz.",
+  },
+  {
+    name: "",
+    caption: "",
+  },
+  {
+    name: "",
+    caption: "",
+  },
+  {
+    name: "",
+    caption: "",
+  },
+  {
+    name: "",
+    caption: "",
+  },
+  {
+    name: "",
+    caption: "",
+  },
+  {
+    name: "",
+    caption: "",
+  },
+  {
+    name: "",
+    caption: "",
+  },
+  {
+    name: "",
+    caption: "",
+  },
+  {
+    name: "",
+    caption: "",
+  },
+  {
+    name: "",
+    caption: "",
+  },
+  {
+    name: "",
+    caption: "",
+  },
+  {
+    name: "",
+    caption: "",
+  },
+  {
+    name: "",
+    caption: "",
+  },
+  {
+    name: "",
+    caption: "",
+  },
+  {
+    name: "",
+    caption: "",
+  },
+  {
+    name: "",
+    caption: "",
+  },
+  {
+    name: "",
+    caption: "",
+  },
+  {
+    name: "",
+    caption: "",
+  },
+  {
+    name: "",
+    caption: "",
+  },
+  {
+    name: "",
+    caption: "",
+  },
+  {
+    name: "",
+    caption: "",
+  },
+  {
+    name: "",
+    caption: "",
+  },
+  {
+    name: "",
+    caption: "",
+  },
+  {
+    name: "",
+    caption: "",
+  },
+  {
+    name: "",
+    caption: "",
+  },
+  {
+    name: "",
+    caption: "",
+  },
+  {
+    name: "",
+    caption: "",
+  },
+  {
+    name: "",
+    caption: "",
+  },
+  {
+    name: "",
+    caption: "",
+  },
+  {
+    name: "",
+    caption: "",
+  },
+];
